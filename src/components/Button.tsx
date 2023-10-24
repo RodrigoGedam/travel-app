2c4febce70e26ac2fdb42dfc7dcdadad4178291c
Rodrigo Gedam <rogedam@gmail.com>
2023-10-24 12:00:15 -0300

    Hero

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 type ButtonProps = {
 	type: "button" | "submit";
 	title: string;
+	variant: string;
 	icon?: string;
-	variant: "btn_dark_green";
 };
 
 export default function Button({ type, title, icon, variant }: ButtonProps) {
 	return (
 		<button
-			type="button"
+			type={type}
 			className={`flexCenter gap-3 rounded-full border ${variant}`}
 		>
 			{icon && <Image src={icon} alt={title} width={25} height={25} />}
