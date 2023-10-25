cf90d34e2cf9c4ef41fe0e02eb16ceb4ef99ac01
Rodrigo Gedam <rogedam@gmail.com>
2023-10-25 17:46:44 -0300

    GetApp

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,16 +5,27 @@ type ButtonProps = {
 	title: string;
 	variant: string;
 	icon?: string;
+	full?: boolean;
 };
 
-export default function Button({ type, title, icon, variant }: ButtonProps) {
+export default function Button({
+	type,
+	title,
+	icon,
+	variant,
+	full,
+}: ButtonProps) {
 	return (
 		<button
 			type={type}
-			className={`flexCenter gap-3 rounded-full border ${variant}`}
+			className={`flexCenter gap-3 rounded-full border ${variant} ${
+				full && "w-full"
+			}`}
 		>
 			{icon && <Image src={icon} alt={title} width={25} height={25} />}
-			<label className="bold whitespace-nowrap">{title}</label>
+			<label className="bold whitespace-nowrap cursor-pointer">
+				{title}
+			</label>
 		</button>
 	);
 }
